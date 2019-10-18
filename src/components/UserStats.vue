5ba18bf58f7d5f76e632fdd4d1f0a26b91ae67a8
Ethan Rockwood <drockwood94@gmail.com>
2019-10-18 01:28:26 -0500

    Add basic API functionality.

---
+++ src/components/UserStats.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="UserStats">
+        <h1>{{ username }}</h1>
+        </h2>{{ userDescription }}</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class UserStats extends Vue {
+  @Prop() private username!: string;
+  @Prop() private userDescription!: string;
+}
+</script>
+
+<style scoped>
+</style>
