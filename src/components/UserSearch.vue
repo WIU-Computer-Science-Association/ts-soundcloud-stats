8b15b2656943996ac1c9033ca3959ea50874cee2
Andrew Kulpa <andrew.kulpa.dev>
2019-10-19 13:20:13 -0500

    Show errors as dismissible alerts, clean up some form code

--- src/components/UserSearch.vue
+++ src/components/UserSearch.vue
@@ -1,25 +1,28 @@
 <template>
-  <b-form v-on:submit.prevent inline >
-    <p v-if="Object.entries(error).length !== 0 && error.constructor === Object">
-      <b>Error with retrieving SoundCloud user data: </b> {{ error }}
-    </p>
-    <label class="sr-only" for="inline-form-input-name">username</label>
-    <b-input-group prepend="@">
-      <b-input id="inline-form-input-username" v-model.trim="username" placeholder="Username"></b-input>
-    </b-input-group>
-    <b-button variant="primary" @click="search()">Search</b-button>
-  </b-form>
+  <b-row class="justify-content-center mt-3">
+    <div class="col-md-8 col-11 justify-content-center">
+      <b-alert v-model="showDismissibleAlert" variant="danger" dismissible>{{ error.message }}</b-alert>
+      <b-form v-on:submit.prevent inline class="col justify-content-center mb-4">
+        <label class="sr-only" for="inline-form-input-name">username</label>
+        <b-input-group prepend="@">
+          <b-input id="inline-form-input-username" v-model.trim="username" placeholder="Username"></b-input>
+        </b-input-group>
+        <b-button variant="primary" @click="search()">Search</b-button>
+      </b-form>
+    </div>
+  </b-row>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Provide, Prop } from 'vue-property-decorator';
+import { Component, Vue, Provide, Prop } from "vue-property-decorator";
 @Component
-export default class VsInput extends Vue {
+export default class UserSearch extends Vue {
   @Provide()
-  public username = '';
+  public username = "";
   @Prop() private error!: object;
+  @Prop() private showDismissibleAlert!: boolean;
   public search() {
-    this.$emit('search', this.username);
+    this.$emit("search", this.username);
   }
 }
 </script>
