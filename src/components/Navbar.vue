fc19a0b37e2784ea4c88975d59be4c4b8e1b1626
Andrew Kulpa <andrew.kulpa.dev>
2019-10-19 13:19:46 -0500

    Make navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">ts-soundcloud-stats</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Provide, Prop } from "vue-property-decorator";
+@Component
+export default class Navbar extends Vue {}
+</script>
