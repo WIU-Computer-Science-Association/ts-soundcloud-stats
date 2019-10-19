80403bfa4743796e302cd72fa5be1f87cc105db0
Andrew Kulpa <andrew.kulpa.dev>
2019-10-19 11:48:24 -0500

    Convert form to bootstrap form

--- src/components/UserSearch.vue
+++ src/components/UserSearch.vue
@@ -1,22 +1,23 @@
 <template>
-  <form v-on:submit.prevent inline class="d-flex justify-content-center">
+  <b-form v-on:submit.prevent inline >
     <p v-if="Object.entries(error).length !== 0 && error.constructor === Object">
       <b>Error with retrieving SoundCloud user data: </b> {{ error }}
     </p>
-    <div class="col-md-6 col-8 pl-0">
-      <input v-model.trim="username" class="w-100 mr-sm-2" type="text" placeholder="Enter Search Term"/>
-    </div>
-    <button class="col-md-6 col-4" @click="search()">Search</button>
-  </form>
+    <label class="sr-only" for="inline-form-input-name">username</label>
+    <b-input-group prepend="@">
+      <b-input id="inline-form-input-username" v-model.trim="username" placeholder="Username"></b-input>
+    </b-input-group>
+    <b-button variant="primary" @click="search()">Search</b-button>
+  </b-form>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Provide, Prop } from 'vue-property-decorator';
 @Component
 export default class VsInput extends Vue {
-  @Prop() private error!: object;
   @Provide()
   public username = '';
+  @Prop() private error!: object;
   public search() {
     this.$emit('search', this.username);
   }
