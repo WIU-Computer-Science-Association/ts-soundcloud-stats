048a35d43fd2ac4b4a22fc14e574a4f356bcad0f
Andrew Kulpa <andrew.kulpa.dev>
2019-10-18 21:58:53 -0500

    Add UserSearch form

---
+++ src/components/UserSearch.vue
@@ -0,0 +1,20 @@
+<template>
+  <form v-on:submit.prevent inline class="d-flex justify-content-center">
+    <div class="col-md-6 col-8 pl-0">
+      <input v-model.trim="username" class="w-100 mr-sm-2" type="text" placeholder="Enter Search Term"/>
+    </div>
+    <button class="my-2 my-sm-0" @click="search()">Search</button>
+  </form>
+</template>
+
+<script lang="ts">
+  import { Component, Vue, Provide } from 'vue-property-decorator'
+  @Component
+  export default class VsInput extends Vue {
+    @Provide()
+    username = '';
+    search() {
+      this.$emit('search', this.username);
+    }
+  }
+</script>
