5f4c5c8c2162152783f1e9005ed09a8e4578ab1a
Andrew Kulpa <andrew.kulpa.dev>
2019-10-20 18:08:32 -0500

    Fix form submission issues

--- src/components/UserSearch.vue
+++ src/components/UserSearch.vue
@@ -2,12 +2,12 @@
   <b-row class="justify-content-center mt-3">
     <div class="col-md-8 col-11 justify-content-center">
       <b-alert v-model="showDismissibleAlert" variant="danger" dismissible>{{ error.message }}</b-alert>
-      <b-form v-on:submit.prevent inline class="col justify-content-center mb-4">
+      <b-form v-on:submit.prevent="search()" inline class="col justify-content-center mb-4">
         <label class="sr-only" for="inline-form-input-name">username</label>
         <b-input-group prepend="@">
           <b-input id="inline-form-input-username" v-model.trim="username" placeholder="Username"></b-input>
         </b-input-group>
-        <b-button variant="primary" @click="search()">Search</b-button>
+        <b-button type="submit" variant="primary">Search</b-button>
       </b-form>
     </div>
   </b-row>
